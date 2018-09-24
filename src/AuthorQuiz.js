c508940a8a39e385450787f7bda71908b8a4a27e
MJ <majid.gorbani@soprasteria.com>
2018-09-24 15:49:54 +0200

    f1

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 
-import Counter from './Counter';
-
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
-function Sum(props) {
-  return <h1>
-    {props.a} + {props.b} = {props.a + props.b}
-  </h1>  
-}
+
+
+
 
 class AuthorQuiz extends Component {
   
@@ -18,8 +14,8 @@ class AuthorQuiz extends Component {
     return (
     <React.Fragment>
       Author Quiz App
-      <Sum a={'key'} b={6} />
-      <Counter />
+
+
     </React.Fragment>
     );
   }
--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
 
-it('renders without crashing', () => {
+it('Author Quiz', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AuthorQuiz />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  
+
 });
