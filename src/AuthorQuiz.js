4384a75b85cb6f88912a0ccbf57aa5369e8a79ba
MJ <majid.gorbani@soprasteria.com>
2018-10-03 22:16:06 +0200

    updated

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
+let Hero = () => {
+  return(
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
 
+        <h1>Author Quiz</h1>
+        <p>select the book written by the autor shown</p>
 
+      </div>
 
+    
+    </div>
+  )
+}
+let Turn = ({authors, books}) =>{
+  return(
+    <div className='row turn' style={{ backgroundColor: "white"}}>
+    <div className='col-4 offset-1'>
+      <img src={authors.imageUrl} className='authorimage' alt='Author' />
+    </div>
+    <div className='col-6'>
+      {books.map((titel)=> <p>{title}</p>)}
+    </div>
 
-class AuthorQuiz extends Component {
-  
+    </div>
+  );
+}
+
+let Continue = ()=>{
+  return(
+    <div></div>
+  );
+}
+let Footer = () =>{
+  return(
+    <div className='row' id='footer'>
+      <div className='col-12'>
+        <p className='text-muted credit'>
+        All images are from <a href='http://commons.wikimedia.org/wiki'>Wiki Media
+        </a>
+        </p>
+      
+      </div>
+    </div>
+  )
+
+}
+
+class AuthorQuiz extends Component {  
   render() {
     return (
     <React.Fragment>
-      Author Quiz App
+      <div className="container-fluid">
+        <Hero />
+        <Turn />
+        <Continue />
+        <Footer />
 
-
+        
+      
+      </div>
     </React.Fragment>
     );
   }
